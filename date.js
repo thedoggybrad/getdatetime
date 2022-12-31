654e54b28b6926625f15ada6df6079a59b919d8b
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-31 17:40:48 +0800

    Test

--- date.js
+++ date.js
@@ -5,3 +5,10 @@ function refreshTime() {
   timeDisplay.textContent = formattedString;
 }
 setInterval(refreshTime, 1000);
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("day").innerHTML = day;
+  setInterval(refreshTime, 1000);
