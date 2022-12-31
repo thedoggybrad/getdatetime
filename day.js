ef3c4c1ba1c772b800845645855b791bf4eae85f
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-31 17:43:15 +0800

    Delete day.js

--- day.js
+++
@@ -1,6 +0,0 @@
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-const d = new Date();
-let day = weekday[d.getDay()];
-document.getElementById("day").innerHTML = day;
-  setInterval(refreshTime, 1000);
