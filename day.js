8d3f328c4d6a19a10f2e9e3aec1e1bcf90cee71c
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-31 17:52:20 +0800

    Update day.js

--- day.js
+++ day.js
@@ -1,4 +1,4 @@
 // Get current day
       let day1 = new Date().toLocaleString('en-US', { weekday: 'long',});
-      document.getElementById("output1").innerHTML = day1; //current day of the week
+      document.getElementById("day").innerHTML = day1; //current day of the week
 setInterval(refreshTime, 1000);
