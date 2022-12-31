5cac4c94872ce4b9d98e478706dd45197924f1ce
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-31 18:00:39 +0800

    Delete date.js

--- date.js
+++
@@ -1,8 +0,0 @@
-`use strict`;
-function refreshTime() {
-  const timeDisplay = document.getElementById("date");
-  const dateString = new Date().toLocaleDateString();
-  const formattedString = dateString.replace(", ", " - ");
-  timeDisplay.textContent = formattedString;
-}
-setInterval(refreshTime, 1000);
