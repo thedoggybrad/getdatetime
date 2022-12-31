a7c5efdbf7e59920400d6f75d2dbfff5b7c5c0e3
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-31 17:44:42 +0800

    Delete time.js

--- time.js
+++
@@ -1,8 +0,0 @@
-`use strict`;
-function refreshTime() {
-  const timeDisplay = document.getElementById("time");
-  const dateString = new Date().toLocaleTimeString();
-  const formattedString = dateString.replace(", ", " - ");
-  timeDisplay.textContent = formattedString;
-}
-  setInterval(refreshTime, 1000);
