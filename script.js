075524fa43c36bb8feccc97518e2dfc4711e0c3a
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-31 17:24:52 +0800

    Isolated the Script of operation via .js

---
+++ script.js
@@ -0,0 +1,24 @@
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleTimeString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
+
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("date");
+  const dateString = new Date().toLocaleDateString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("day").innerHTML = day;
+  setInterval(refreshTime, 1000);
